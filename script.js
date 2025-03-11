ffce69fb3b86d090e9b3b4097824f7dcdbd8ef55
nobregavictor <victornobrega78@gmail.com>
2025-03-10 22:59:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("mudarCor").addEventListener("click", function(){
+
+    const corAleatoria = Math.floor(Math.random()*16777215).toString(16);
+
+    document.body.style.backgroundColor = "#" + corAleatoria;
+
+})
